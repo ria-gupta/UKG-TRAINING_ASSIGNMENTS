3f6a6e8c1f5f4216cd653005871c93ee7a0e47b1
ria.gupta <ria.gupta@IN-4LHQX33.KRONOS.com>
2020-09-18 21:12:14 +0530

    Day2 commit

---
+++ Lab2Excercises/src/Que2/BookStore.java
@@ -0,0 +1,54 @@
+package Que2;
+
+import java.util.*;
+
+class BookStore {
+
+	private Book[] book = new Book[10];
+
+	public void sell(String bookTitle, int quantity) {
+		Book selectBook = null;
+		for (Book temp : book) {
+			if (temp != null && temp.getBookTitle().equals(bookTitle)) {
+				selectBook = temp;
+				break;
+			}
+
+		}
+		if (selectBook != null)
+			selectBook.setCopies(Math.max(0, selectBook.getCopies() - quantity));
+		else
+			System.out.println("Book currently unavailable!!");
+
+	}
+
+	public void order(String isbn, int quantity) {
+		for (Book temp : book) {
+			if (temp != null && temp.getISBN().equals(isbn)) {
+				temp.setCopies(temp.getCopies() + quantity);
+				break;
+			} else if (temp == null) {
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Book not found!");
+				System.out.print("Enter the title of the book");
+				String title = sc.nextLine();
+				System.out.print("Enter the author of the book");
+				String author = sc.nextLine();
+				book[Arrays.asList(book).indexOf(temp)]= new Book(title, author, isbn, quantity);
+				sc.close();
+				break;
+
+			}
+
+		}
+
+	}
+
+	public void display() {
+		for (Book temp : book)
+			if (temp != null)
+				temp.display();
+
+	}
+
+}
---
+++ Lab2Excercises/src/Que5/Car.java
@@ -0,0 +1,28 @@
+package Que5;
+
+class Car extends Vehicle {
+	private int noOfDoor;
+	
+	
+	public Car(int noOfWheels, int noOfPassenger, int model, String make, int noOfDoor) {
+		super(noOfWheels, noOfPassenger, model, make);
+		this.noOfDoor = noOfDoor;
+	}
+
+
+
+
+	public void setNoOfDoor(int noOfDoor) {
+		this.noOfDoor = noOfDoor;
+	}
+
+
+	public void display()
+	{
+		System.out.println("MAKE :"+this.getMake());
+		System.out.println("MODEL :"+this.getModel());
+		System.out.println("NO OF DOORs :"+noOfDoor);
+	}
+	
+
+}
